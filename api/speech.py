43391e80c0ebaa56f09c1de5af54cf632caaefa9
Poxios <poxios0310@gmail.com>
2023-07-25 13:13:41 +0900

    analysis1 주석 추가

--- api/speech.py
+++ api/speech.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import concurrent.futures
+
 
 app = FastAPI()
 
 
 class Analysis1(BaseModel):
+    '''
+    ## 클로바로 넘겨줄 값
+    callback_url : stt 결과를 전송할 dalcom endpoint
+    download_url : stt 입력 파일이 저장된 object의 presigned url
+    ## wasak에서 쓸 값
+    upload_url : wasak에서 합친 오디오 파일(full audio)을 업로드할 presigned url @
+    '''
     callback_url: str
     upload_url: str
     download_url: str
@@ -12,6 +21,7 @@ class Analysis1(BaseModel):
 
 @app.post("/{speech_id}/analysis-1")
 def trigger_analysis_1(dto: Analysis1):
+    concurrent
     print(dto)
     return None
 
