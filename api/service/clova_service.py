0e05b5dd5bd7fe4f2ab1759c01d48df6c74535c1
pcjs156 <qkrdbqls1001@gmail.com>
2023-08-01 11:35:57 +0900

    test: STT 디버깅용 로그 코드 추가

--- api/service/clova_service.py
+++ api/service/clova_service.py
@@ -10,15 +10,22 @@ def clova_stt_send(audio_file_path: Path, response_callback_url: str):
         "url": str(audio_file_path),  # 분석할 음성 파일 URL
         "language": "ko-KR",
         "callback": response_callback_url,  # 결과 송신받을 콜백 URL
+        "resultToObs": True,
     }
 
     headers = {"X-CLOVASPEECH-API-KEY": clova_speech_config.clova_secret_key}
 
-    response = requests.post(
+    print(
+        "clova_speech_config.clova_stt_target_url:",
+        clova_speech_config.clova_stt_target_url,
+    )
+    print("request_body:", request_body)
+    response: requests.Response = requests.post(
         clova_speech_config.clova_stt_target_url, json=request_body, headers=headers
     )
 
     if response.status_code == 200:
+        print(response.json())
         return True
     else:
         return False
