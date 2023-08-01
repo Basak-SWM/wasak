648b5c82814876273c690c22e12e263e57ad7595
Poxios <poxios0310@gmail.com>
2023-08-01 16:47:18 +0900

    added typehint

--- api/service/aws/s3.py
+++ api/service/aws/s3.py
@@ -59,7 +59,7 @@ class S3Service:
             object_key (str): 다운로드할 파일의 이름
 
         Returns:
-            Any: 다운로드된 JSON object
+            Any(정상적으로 stt 결과 로드되었다면 dict): 다운로드된 JSON object
         """
         bucket_name = self.get_default_bucket_name()
         response = self.client.get_object(Bucket=bucket_name, Key=object_key)
