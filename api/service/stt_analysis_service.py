037624970f51145e2f351f682315de87fbed1090
Poxios <poxios0310@gmail.com>
2023-08-02 23:19:09 +0900

    문장별 lpm 최적화

--- api/service/stt_analysis_service.py
+++ api/service/stt_analysis_service.py
@@ -2,30 +2,25 @@ import json
 from typing import Tuple, List
 
 
-def get_average_wpm(stt_json: str) -> int:
+def get_average_lpm(stt_json: dict) -> int:
     """
     _summary_
-        STT 결과를 분석하여 WPM을 계산한다.
+        STT 결과를 분석하여 LPM (Letters per minute)을 계산한다.
 
     Args:
         stt_json (str): Clova에서 받은 STT 결과
 
     Returns:
-        LPM (Letter per minute): 분당 음절 수
+        LPM (Letters per minute): 분당 음절 수
     """
-    stt_result = json.loads(stt_json)
-
-    letters_count = 0
-
-    # 스크립트의 전체 음절 수 더하기
-    for segment in stt_result["segments"]:
-        letters_count += len(segment["text"])
+    # FIXME: 평균 LPM이 문장별 LPM의 평균을 의미하는지, 전체 음성의 LPM을 의미하는지 결정 필요
+    only_letters_text = stt_json["text"].replace(" ", "").replace(".", "")
 
     # <분당 음절 수 계산>
     # 느림: 300음절 / min
     # 보통: 350음절 / min
     # 빠름: 400음절 / min
-    return letters_count / (stt_result["segments"][-1]["end"] / 1000 / 60)
+    return len(only_letters_text) / (stt_json["segments"][-1]["end"] / 1000 / 60)
 
 
 def get_lpm_by_sentense(stt_json: str) -> List[Tuple[int, int, str]]:
@@ -34,35 +29,19 @@ def get_lpm_by_sentense(stt_json: str) -> List[Tuple[int, int, str]]:
         모든 segments 들을 순회하며 words 별로 걸린 시간 및 글자 수를 합산, '.'를 만나면 문장의 끝으로 판단하여 lpm_by_sentense를 계산한다.
 
     Args:
-        stt_json (str): Clova에서 받은 STT 결과
+        stt_json (dict): Clova에서 받은 STT 결과를 reconstruct한 json
 
     Returns:
-        문장별 lpm 분석을 속도가 빠른 순서대로 반환.
-        [
-            (start_time, end_time, lpm),
-        ]
+        List[int]: 문장별 lpm 분석 결과를 기존 index에 맞춰 반환
+
     """
-    stt_result = json.loads(stt_json)
 
-    lpm_by_sentense = []
-
-    letters_count = 0
-    start_time = -1
-
-    for segment in stt_result["segments"]:
-        for start, end, word in segment["words"]:
-            start_time = start if start_time == -1 else start_time
-            letters_count += len(word)
-            # TODO: 기호로 문장의 끝을 판단했으나 다른 기호도 존재하는지 확인 필요
-            if "." in word or "?" in word or "!" in word:
-                letters_count -= 1
-                lpm = letters_count / ((end - start_time) / 1000 / 60)
-                lpm_by_sentense.append([start_time, end, lpm])
-                letters_count = 0
-                start_time = -1
-
-    # lpm 순서대로 정렬
-    lpm_by_sentense.sort(key=lambda x: x[2], reverse=True)
+    lpm_by_sentense = [
+        len(s["text"].replace(" ", "").replace(".", ""))
+        / (s["end"] - s["start"])
+        * 1000
+        for s in stt_json["segments"]
+    ]
 
     return lpm_by_sentense
 
