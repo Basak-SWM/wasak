db71bbde6cae1326650476a43bb686d103d74e65
pcjs156 <qkrdbqls1001@gmail.com>
2023-08-08 20:45:04 +0900

    hotfix: json.loads 타입 변화 분기 추가

--- api/controller/speech.py
+++ api/controller/speech.py
@@ -178,7 +178,9 @@ def analysis2_async_wrapper(presentation_id: int, speech_id: int):
 
     # 1. S3에서 p.id / s.id로 STT 결과 json을 받아온다.
     stt_key = f"{presentation_id}/{speech_id}/analysis/STT.json"
-    stt_script = json.loads(s3_service.download_json_object(stt_key))
+    stt_script = s3_service.download_json_object(stt_key)
+    if not isinstance(stt_script, dict):
+        stt_script = json.loads(stt_script)
 
     # 2. STT 결과를 kiwi를 이용하여 문장 별로 분할하여 재조합한다.
     concatenated_script = speech_service.get_aligned_script(stt_script)
