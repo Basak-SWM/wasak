cf6250bde1f83a8d887ec8fb97d10ff6cb9b5922
pcjs156 <qkrdbqls1001@gmail.com>
2023-11-22 15:58:01 +0900

    hotfix: Allow all cors origins

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 from api.controller.info import app as info_app
 from api.controller.speech import app as speech_app
 from api.controller.chatbot import app as chatbot_app
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/api/v1/info", info_app)
 app.mount("/api/v1/presentations", speech_app)
